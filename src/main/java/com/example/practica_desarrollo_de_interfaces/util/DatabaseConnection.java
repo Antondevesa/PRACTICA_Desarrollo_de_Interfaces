f847205f68662b1211260ca523ad703de310e98c
Antonio Devesa <antondevesa03@gmail.com>
2026-05-01 16:34:41 +0200

    Implementación de la conexión a la base de datos MySQL

---
+++ src/main/java/com/example/practica_desarrollo_de_interfaces/util/DatabaseConnection.java
@@ -0,0 +1,27 @@
+package com.example.practica_desarrollo_de_interfaces.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static DatabaseConnection instance;
+    private static final String URL = "jdbc:mysql://localhost:3306/recuperacion_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    private DatabaseConnection() {
+    }
+
+    public static DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.practica_desarrollo_de_interfaces {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires java.sql;
 
 
     opens com.example.practica_desarrollo_de_interfaces to javafx.fxml;
